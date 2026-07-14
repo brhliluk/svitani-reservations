c0525c35c183275db0e5967280106f78862f5a72
lukas.brhlik <lukas.brhlik@juicymo.cz>
2026-07-14 22:30:33 +0200

    Fix ShowAttendeeCountCheckbox value

--- src/webMain/kotlin/cz/svitaninymburk/projects/reservations/ui/admin/events/EventFormComponents.kt
+++ src/webMain/kotlin/cz/svitaninymburk/projects/reservations/ui/admin/events/EventFormComponents.kt
@@ -269,7 +269,7 @@ fun IComponent.ShowAttendeeCountCheckbox(
         p(className = "label-text font-medium mb-1") { +currentStrings.showAttendeeCount }
         label(className = "cursor-pointer label justify-start gap-3") {
             checkBox(value = value, className = "checkbox checkbox-primary") {
-                onChange { onValueChange(value) }
+                onChange { onValueChange(this.value) }
             }
             span(className = "label-text text-sm text-base-content/70") { +currentStrings.showAttendeeCountHint }
         }
